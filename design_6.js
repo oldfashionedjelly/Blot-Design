0410c3bfa664b6fafaf4cc75f1c7bb855c8d1ec2
Alexandra Bunch <121991056+oldfashionedjelly@users.noreply.github.com>
2024-07-14 16:26:19 -0700

    Update design_6.js

--- design_6.js
+++ design_6.js
@@ -27,7 +27,7 @@ function getRandomAngle() {
 
 // number of stars and size of stars
 const numStars = (10*Math.random())+10;
-const starRadius = 0.8;
+const starRadius = 0.4;
 
 // store final lines here
 const finalLines = [];
@@ -65,8 +65,8 @@ function createStars(numStars, minX, maxX, minY, maxY, radius, avoidCenter, avoi
     do {
       x = Math.random() * (maxX - minX) + minX;
       y = Math.random() * (maxY - minY) + minY;
-    } while ((y<91 && y>32) || (avoidCenter && Math.sqrt((x - avoidCenter[0]) ** 2 + (y - avoidCenter[1]) ** 2) < avoidRadius));
-    const star = createCircle([x, y], radius, 10);
+    } while ((y<92 && y>31) || (avoidCenter && Math.sqrt((x - avoidCenter[0]) ** 2 + (y - avoidCenter[1]) ** 2) < avoidRadius));
+    const star = createCircle([x, y], (radius*Math.random())+0.6, 10);
     stars.push(star);
   }
   return stars;
@@ -81,12 +81,12 @@ rings.forEach(ring => {
 });
 
 // add random stars around Saturn, avoiding the center
-const minX = 0;
-const maxX = width;
-const minY = 0;
-const maxY = height;
+const minX = 2;
+const maxX = width-2;
+const minY = 2;
+const maxY = height-2;
 const avoidCenter = saturn.center;
-const avoidRadius = planetRadius;
+const avoidRadius = planetRadius+2;
 const stars = createStars(numStars, minX, maxX, minY, maxY, starRadius, avoidCenter, avoidRadius);
 finalLines.push(...stars);
 
@@ -111,7 +111,9 @@ function createEllipse(center, radiusX, radiusY, angle, numPoints = 200) {
     const dist = Math.sqrt((rotatedX - 62.5) ** 2 + (rotatedY - 62.5) ** 2);
 
     if (dist > planetRadius || rotatedY < 62.5) {
-      segment.push([rotatedX, rotatedY]);
+      if(rotatedX>=0&&rotatedX<=width) {
+        segment.push([rotatedX, rotatedY]);
+      }
     } else {
       if (segment.length > 0) {
         points.push(segment);
